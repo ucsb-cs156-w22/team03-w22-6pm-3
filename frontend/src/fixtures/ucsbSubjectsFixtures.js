0ca50f5b3134305a5a1bfcb3dbf8e8fed1556b33
Ryan Mosalem <ryanmosalem@ucsb.edu>
2022-02-21 11:36:46 -0800

    finished UCSBSubjects table

---
+++ frontend/src/fixtures/ucsbSubjectsFixtures.js
@@ -0,0 +1,43 @@
+const ucsbSubjectsFixtures = {
+    oneSubject: {
+        "id": 1,
+        "subjectCode": "test1",
+        "subjectTranslation": "x",
+        "deptCode": "1",
+        "collegeCode": "1",
+        "relatedDeptCode": "1",
+        "inactive": "false"
+    },
+    threeSubjects: [
+        {
+            "id": 1,
+            "subjectCode": "test1",
+            "subjectTranslation": "x",
+            "deptCode": "1",
+            "collegeCode": "1",
+            "relatedDeptCode": "1",
+            "inactive": "false"
+        },
+        {
+            "id": 2,
+            "subjectCode": "test2",
+            "subjectTranslation": "y",
+            "deptCode": "2",
+            "collegeCode": "2",
+            "relatedDeptCode": "2",
+            "inactive": "false"
+        },
+        {
+            "id": 3,
+            "subjectCode": "test3",
+            "subjectTranslation": "z",
+            "deptCode": "3",
+            "collegeCode": "3",
+            "relatedDeptCode": "3",
+            "inactive": "false"
+        }
+    ]
+};
+
+
+export { ucsbSubjectsFixtures };
