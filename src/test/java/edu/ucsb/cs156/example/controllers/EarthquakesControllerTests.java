82750617f6778a255c163ba767ea521cae3bb48f
gama-A <59851451+gama-A@users.noreply.github.com>
2022-02-22 18:50:40 -0800

    ga - completed controller, working on test

--- src/test/java/edu/ucsb/cs156/example/controllers/EarthquakesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/EarthquakesControllerTests.java
@@ -9,6 +9,8 @@ import edu.ucsb.cs156.example.collections.StudentCollection;
 import edu.ucsb.cs156.example.documents.EQfeature;
 import edu.ucsb.cs156.example.documents.EQgeometry;
 import edu.ucsb.cs156.example.documents.EQproperties;
+import edu.ucsb.cs156.example.documents.EQmetadata;
+import edu.ucsb.cs156.example.documents.EQlisting;
 import edu.ucsb.cs156.example.documents.RedditPost;
 import edu.ucsb.cs156.example.documents.RedditT3;
 import edu.ucsb.cs156.example.documents.Student;
@@ -131,5 +133,74 @@ public class EarthquakesControllerTests extends ControllerTestCase {
 
             verify(earthquakesCollection, times(1)).deleteAll();
     }   
+
+    @WithMockUser(roles = { "ADMIN" })
+    @Test
+    public void api_retrieve_eqfeatures_is_void() throws Exception {
+            EQproperties properties = EQproperties.builder()
+            .mag(6.5)
+            .place("test")
+            .time(0x4000000L)
+            .updated(0x5000L)
+            .tz(8)
+            .url("test")
+            .detail("test")
+            .felt(1)
+            .cdi(1.1)
+            .mmi(1.1)
+            .alert("test")
+            .status("test")
+            .tsunami(1)
+            .sig(1)
+            .net("test")
+            .code("test")
+            .ids("test")
+            .sources("test")
+            .types("test")
+            .nst(1)
+            .dmin(0.1)
+            .rms(0.1)
+            .gap(0.1)
+            .magType("test")
+            .type("test")
+            .build();
+
+            List<EQproperties> lep = new ArrayList<>();
+            lep.add(properties);
+
+            EQgeometry geometry = EQgeometry.builder()
+            .type("Point")
+            .coordinates(List.of(1.0,1.0,1.0))
+            .build();
+
+            List<EQgeometry> leg = new ArrayList<>();
+            leg.add(geometry);
+
+            EQfeature feature = EQfeature.builder()
+            ._id("a")
+            .type("test")
+            .properties(properties)
+            .geometry(geometry)
+            .id("test")
+            .build();
+
+            List<EQfeature> lef = new ArrayList<>();
+            lef.add(feature);
+
+            EQmetadata md = EQmetadata.builder()
+                // stub
+
+            EQlisting el = EQlisting.builder()
+                .type("EQlisting")
+                .metadata(md)
+                // stub
+
+            mockMvc.perform(post("/api/earthquakes/retrieve").with(csrf())).andExpect(status().isOk()).andReturn();
+
+            verify(earthquakesCollection, times(1)).saveAll(features);
+            String expectedJson = mapper.writeValueAsString(lef);
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(expectedJson, responseString);
+    }
 
 }
