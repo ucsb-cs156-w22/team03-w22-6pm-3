a8db6bfc98da45c6e986df0916029608bc2fdca3
Ryan Mosalem <ryanmosalem@ucsb.edu>
2022-02-27 20:28:28 -0800

    rm - update

--- frontend/src/fixtures/ucsbSubjectsFixtures.js
+++ frontend/src/fixtures/ucsbSubjectsFixtures.js
@@ -1,42 +1,42 @@
 const ucsbSubjectsFixtures = {
-    oneSubject: {
-        "id": 1,
-        "subjectCode": "test1",
-        "subjectTranslation": "x",
-        "deptCode": "1",
-        "collegeCode": "1",
-        "relatedDeptCode": "1",
-        "inactive": "false"
-    },
-    threeSubjects: [
-        {
-            "id": 1,
-            "subjectCode": "test1",
-            "subjectTranslation": "x",
-            "deptCode": "1",
-            "collegeCode": "1",
-            "relatedDeptCode": "1",
-            "inactive": "false"
-        },
-        {
-            "id": 2,
-            "subjectCode": "test2",
-            "subjectTranslation": "y",
-            "deptCode": "2",
-            "collegeCode": "2",
-            "relatedDeptCode": "2",
-            "inactive": "false"
-        },
-        {
-            "id": 3,
-            "subjectCode": "test3",
-            "subjectTranslation": "z",
-            "deptCode": "3",
-            "collegeCode": "3",
-            "relatedDeptCode": "3",
-            "inactive": "false"
-        }
-    ]
+  oneSubject: {
+      "id": 1,
+      "subjectCode": "ANTH",
+      "subjectTranslation": "Anthropology",
+      "deptCode": "ANTH",
+      "collegeCode": "L&S",
+      "relatedDeptCode": "null",
+      "inactive": "false"
+  },
+  threeSubjects: [
+      {
+          "id": 1,
+          "subjectCode": "ANTH",
+          "subjectTranslation": "Anthropology",
+          "deptCode": "ANTH",
+          "collegeCode": "L&S",
+          "relatedDeptCode": "null",
+          "inactive": "false"
+      },
+      {
+          "id": 2,
+          "subjectCode": "CMPSC",
+          "subjectTranslation": "Computer Science",
+          "deptCode": "CMPSC",
+          "collegeCode": "COE",
+          "relatedDeptCode": "null",
+          "inactive": "false"
+      },
+      {
+          "id": 3,
+          "subjectCode": "PHIL",
+          "subjectTranslation": "Philosophy",
+          "deptCode": "PHIL",
+          "collegeCode": "L&S",
+          "relatedDeptCode": "null",
+          "inactive": "true"
+      }
+  ]
 };
 
 
