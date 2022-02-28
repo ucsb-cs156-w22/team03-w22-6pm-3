eefe1178a1a29535a799463d35960b427e6c39bc
gama-A <59851451+gama-A@users.noreply.github.com>
2022-02-28 13:26:45 -0800

    ga - fixing last test issue

--- frontend/src/fixtures/earthquakesFixtures.js
+++ frontend/src/fixtures/earthquakesFixtures.js
@@ -12,18 +12,22 @@ const earthquakesFixtures = {
     },
     twoEarthquakes: [
        {
+         properties:{
         "id": "abcd1234abcd1234abcd1234",
         "title": "M 2.2 - 10km ESE of Ojai, CA",
         "mag": 2.16,
         "place": "10km ESE of Ojai, CA",
         "time": 1644571919000,
+         },
        },
        {
+         properties: {
         "id": "abcd5678abcd5678abcd5678",
         "title": "M 2.6 - 5km ESE of Ojai, CA",
         "mag": 2.6,
         "place": "5km ESE of Ojai, CA",
         "time": 1644571918123,
+         },
        }
      ]
    }
