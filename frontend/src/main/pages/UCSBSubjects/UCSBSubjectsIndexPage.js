af3f9a9ec88bd29da1fc540c8045e25d06287920
Ryan Mosalem <ryanmosalem@ucsb.edu>
2022-02-27 20:37:24 -0800

    rm - updating list

--- frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
+++ frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
@@ -5,6 +5,8 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UCSBSubjectsTable from 'main/components/UCSBSubjects/UCSBSubjectsTable';
 import { useCurrentUser } from 'main/utils/currentUser'
 
+import { subjectFixtures } from "fixtures/ucsbSubjectsFixtures";
+
 export default function UCSBSubjectsIndexPage() {
 
   const currentUser = useCurrentUser();
@@ -12,8 +14,8 @@ export default function UCSBSubjectsIndexPage() {
   const { data: subjects, error: _error, status: _status } =
     useBackend(
       // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/ucsbsubjects/all"],
-      { method: "GET", url: "/api/ucsbsubjects/all" },
+      ["/api/UCSBSubjects/all"],
+      { method: "GET", url: "/api/UCSBSubjects/all" },
       []
     );
 
